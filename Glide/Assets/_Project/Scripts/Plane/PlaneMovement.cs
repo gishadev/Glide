bad14b1d1af424436ea47d98aa6f4393067410ec
GishaTheDeveloper <spaceshhz@gmail.com>
2020-12-20 15:47:47 +0200

    add 1-dir movement for player's plane

---
+++ Glide/Assets/_Project/Scripts/Plane/PlaneMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide
+{
+    public class PlaneMovement : MonoBehaviour
+    {
+        [SerializeField] private float forwardMovement = default;
+
+        Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();   
+        }
+
+        private void Update()
+        {
+            ForwardMovement();
+        }
+
+        void ForwardMovement()
+        {
+            _rb.velocity = Vector3.forward * forwardMovement * Time.fixedDeltaTime;
+        }
+    }
+}
