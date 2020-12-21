eb2bb1ef41fef1d599e0ddce5fa766d93063228a
GishaTheDeveloper <spaceshhz@gmail.com>
2020-12-21 18:32:03 +0200

    small changes to camera controller

--- Glide/Assets/_Project/Scripts/Plane/CameraMovement.cs
+++ Glide/Assets/_Project/Scripts/Plane/CameraMovement.cs
@@ -4,29 +4,6 @@ namespace Gisha.Glide.Plane
 {
     public class CameraMovement : MonoBehaviour
     {
-        [Header("Following Settings")]
-        [SerializeField] private Transform followTarget = default;
-        [SerializeField] private float followSpeed = default;
-        [SerializeField] private Vector3 followOffset = default;
 
-        Transform _transform;
-
-        private void Awake()
-        {
-            _transform = transform;
-        }
-
-        private void FixedUpdate()
-        {
-            Following();
-        }
-
-        void Following()
-        {
-            var oldPos = _transform.position;
-            var newPos = new Vector3(followTarget.position.x, followTarget.position.y, followTarget.position.z) + followOffset; 
-
-            _transform.position = Vector3.Lerp(oldPos, newPos, followSpeed * Time.deltaTime);
-        }
     }
 }
