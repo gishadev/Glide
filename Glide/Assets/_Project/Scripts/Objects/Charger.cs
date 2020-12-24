d47def996568ae682f1db1f246b465143b2ed0ad
GishaTheDeveloper <spaceshhz@gmail.com>
2020-12-25 00:09:41 +0200

    add charging through chargers

---
+++ Glide/Assets/_Project/Scripts/Objects/Charger.cs
@@ -0,0 +1,17 @@
+﻿using Gisha.Glide.AirplaneGeneric;
+using UnityEngine;
+
+namespace Gisha.Glide.Objects
+{
+    public class Charger : TriggerableObject
+    {
+        [Header("Charger")]
+        [SerializeField] private GameObject chargingGate = default;
+
+        public override void OnTriggerSignal(Collider other)
+        {
+            other.GetComponentInParent<Airplane>().ChargeUp();
+            chargingGate.SetActive(false);
+        }
+    }
+}
---
+++ Glide/Assets/_Project/Scripts/Objects/TriggerableObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide.Objects
+{
+    public abstract class TriggerableObject : MonoBehaviour
+    {
+        [Header("Triggerable Object Settings")]
+        [SerializeField] private TriggerSignal triggerSignal = default;
+
+        private void OnEnable()
+        {
+            triggerSignal.OnTriggerSignal += OnTriggerSignal;
+        }
+
+        private void OnDisable()
+        {
+            triggerSignal.OnTriggerSignal -= OnTriggerSignal;
+        }
+
+        public abstract void OnTriggerSignal(Collider other);
+    }
+}
