ba8803759390e093122b23680524616060702d6d
GishaTheDeveloper <spaceshhz@gmail.com>
2020-12-25 18:37:02 +0200

    add additive scenes

--- Glide/Assets/_Project/Scripts/Game/GameManager.cs
+++ Glide/Assets/_Project/Scripts/Game/GameManager.cs
@@ -1,10 +1,52 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 namespace Gisha.Glide.Game
 {
-    public static class GameManager
+    public class GameManager : MonoBehaviour
     {
-        public static void ReloadScene() => SceneManager.LoadScene(0);
+        #region Singleton
+        public static GameManager Instance { get; private set; }
+        #endregion
+
+        public const string relativePath = "_Project/Scenes";
+
+        public string gameScene;
+        public string[] levelScenes;
+
+        private void Awake()
+        {
+            CreateInstance();
+
+            if (gameScene == null) Debug.LogError("You haven't selected gameScene!");
+            if (levelScenes == null || levelScenes.Length == 0) Debug.LogError("You haven't selected any levelScene!");
+        }
+
+        private void Start()
+        {
+            ReloadScene();
+        }
+
+        private void CreateInstance()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            if (Instance == null)
+                Instance = this;
+
+            if (Instance != null && Instance != this)
+                Destroy(gameObject);
+        }
+
+        private static string GetPathToScene(string sceneName) => $"{relativePath}/{sceneName}";
+
+        private static void LoadLevel(int index)
+        {
+            SceneManager.LoadScene(GetPathToScene(Instance.gameScene));
+            SceneManager.LoadScene(GetPathToScene(Instance.levelScenes[index]), LoadSceneMode.Additive);
+        }
+
+        public static void ReloadScene() => LoadLevel(0);
     }
 }
