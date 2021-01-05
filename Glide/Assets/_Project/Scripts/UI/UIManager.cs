cacded132adb1f92b177d10afe79269963d87b06
GishaTheDeveloper <spaceshhz@gmail.com>
2021-01-05 15:41:11 +0200

    add UIManager and call levels menu on ESCAPE

---
+++ Glide/Assets/_Project/Scripts/UI/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [Header("General")]
+        [SerializeField] private GameObject levelsMenu = default;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                levelsMenu.SetActive(!levelsMenu.activeSelf);
+        }
+    }
+}
