dc31d4aaf80ee0a28233fb06a853c7380ca114af
GishaTheDeveloper <spaceshhz@gmail.com>
2021-01-08 20:42:53 +0200

    add rotator for universe

---
+++ Glide/Assets/_Project/Scripts/MainMenu/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide.MainMenu
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float speed = default;
+
+        Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _transform.Rotate(Vector3.forward * speed);
+        }
+    }
+}
