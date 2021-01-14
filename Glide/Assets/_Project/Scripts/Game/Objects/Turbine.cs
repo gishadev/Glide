04480c51eb2cfb2d9451b903929583bb94d6024e
GishaTheDeveloper <spaceshhz@gmail.com>
2021-01-14 23:44:19 +0200

    redesign level 5, add turbines

---
+++ Glide/Assets/_Project/Scripts/Game/Objects/Turbine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide.Game.Objects
+{
+    public class Turbine : MonoBehaviour
+    {
+        [Header("General")]
+        [SerializeField] private float angularSpeed = default;
+
+        Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _transform.Rotate(Vector3.forward * angularSpeed * Time.deltaTime);
+        }
+
+    }
+}
