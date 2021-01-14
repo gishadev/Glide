ef729fb77f5312c4960d0d50ba437b007dce209a
GishaTheDeveloper <spaceshhz@gmail.com>
2021-01-14 18:25:01 +0200

    improve module collectible look

---
+++ Glide/Assets/_Project/Scripts/Game/Objects/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide.Game.Objects
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        Transform _transform;
+        Camera _cam;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _cam = Camera.main;
+        }
+
+        private void Update()
+        {
+            _transform.LookAt(_cam.transform);
+        }
+    }
+}
