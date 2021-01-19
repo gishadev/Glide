a7c96d25a7ed800efbbcf6ebe74e39684a3c8e8a
GishaTheDeveloper <spaceshhz@gmail.com>
2021-01-19 21:37:14 +0200

    improve triggerable objects rotation towards airplane

--- Glide/Assets/_Project/Scripts/Game/Objects/Charger.cs
+++ Glide/Assets/_Project/Scripts/Game/Objects/Charger.cs
@@ -8,35 +8,12 @@ namespace Gisha.Glide.Game.Objects
     {
         [Header("Charger")]
         [SerializeField] private GameObject chargingGate = default;
-        [SerializeField] private float minDistToRotatateTowardsAirplane = default;
-
-        Transform _transform;
-        Airplane _airplane;
 
         public override void OnTriggerSignal(Collider other)
         {
             other.GetComponentInParent<Airplane>().chargeController.ChargeUp();
             ScoreProcessor.AddScore(50);
             chargingGate.SetActive(false);
-        }
-
-        private void Start()
-        {
-            _transform = transform;
-            _airplane = AirplaneSpawner.Instance.Airplane;
-        }
-
-        private void Update()
-        {
-            if (!IsTriggered && Vector3.Distance(_transform.position, _airplane.transform.position) > minDistToRotatateTowardsAirplane)
-                LootAtAirplane();
-        }
-
-        private void LootAtAirplane()
-        {
-            var forward = (_airplane.transform.position - _transform.position).normalized;
-            var upwards = Vector3.up;
-            _transform.rotation = Quaternion.LookRotation(forward, upwards);
         }
     }
 }
---
+++ Glide/Assets/_Project/Scripts/Game/Objects/LookAtAirplane.cs
@@ -0,0 +1,35 @@
+﻿using Gisha.Glide.Game.AirplaneGeneric;
+using UnityEngine;
+
+namespace Gisha.Glide.Game.Objects
+{
+    [RequireComponent(typeof(TriggerableObject))]
+    public class LookAtAirplane : MonoBehaviour
+    {
+        [Header("Rotate Toward Airplane")]
+        [SerializeField] private Transform rotationTarget = default;
+        [SerializeField] private float minDistToRotateTowardsAirplane = default;
+
+        Airplane _airplane;
+        TriggerableObject _trigger;
+
+        private void Start()
+        {
+            _airplane = AirplaneSpawner.Instance.Airplane;
+            _trigger = GetComponent<TriggerableObject>();
+        }
+
+        private void Update()
+        {
+            if (!_trigger.IsTriggered && Vector3.Distance(rotationTarget.position, _airplane.transform.position) > minDistToRotateTowardsAirplane)
+                LootAtAirplane();
+        }
+
+        private void LootAtAirplane()
+        {
+            var forward = (_airplane.transform.position - rotationTarget.position).normalized;
+            var upwards = Vector3.up;
+            rotationTarget.rotation = Quaternion.LookRotation(forward, upwards);
+        }
+    }
+}
