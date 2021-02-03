53ed347b16829209600f99f1b668dd9d89205950
GishaTheDeveloper <spaceshhz@gmail.com>
2021-02-04 00:24:52 +0200

    add basic turret behaviour

---
+++ Glide/Assets/_Project/Scripts/Game/Objects/Shell.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Gisha.Glide.Game.Objects
+{
+    public class Shell : MonoBehaviour
+    {
+        [Header("General")]
+        [SerializeField] private float flyingSpeed = default;
+        [SerializeField] private float lifeTime = default;
+
+        Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            Invoke("Explode", lifeTime);
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = transform.forward * flyingSpeed * Time.fixedDeltaTime;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("Obstacle") || collision.collider.CompareTag("Airplane"))
+                Explode();
+        }
+
+        private void Explode()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Glide/Assets/_Project/Scripts/Game/Objects/Turret.cs
@@ -0,0 +1,62 @@
+﻿using Gisha.Glide.Game.AirplaneGeneric;
+using System.Collections;
+using UnityEngine;
+
+namespace Gisha.Glide.Game.Objects
+{
+    public class Turret : MonoBehaviour
+    {
+        [Header("General")]
+        [SerializeField] private GameObject projectilePrefab = default;
+        [SerializeField] private float shootingDelay = default;
+
+        Collider _collider;
+        Transform _transform;
+        Airplane _airplane;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+            _transform = transform;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(ShootingCoroutine());
+            _airplane = AirplaneSpawner.Instance.Airplane;
+        }
+
+        private void Update()
+        {
+            LookAtAirplane();
+        }
+
+        private IEnumerator ShootingCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(shootingDelay);
+
+                var layerMask = 1 << LayerMask.NameToLayer("Airplane");
+                if (Physics.Raycast(_transform.position, _transform.forward, out RaycastHit hitInfo, 2500f, layerMask))
+                {
+                    if (hitInfo.collider.CompareTag("Airplane"))
+                        Shoot();
+
+                    Debug.Log(hitInfo.collider.name);
+                }
+
+            }
+        }
+
+        private void Shoot()
+        {
+             GameObject.Instantiate(projectilePrefab, _transform.position, _transform.rotation);
+        }
+
+        private void LookAtAirplane()
+        {
+            _transform.LookAt(_airplane.transform);
+        }
+    }
+}
